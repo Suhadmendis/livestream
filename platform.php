dbf7baae774724fc300d5fc0c83fe9f1f472ce35
Suhadmendis <suhad.a.mendis@gmail.com>
2020-08-26 15:22:05 +0530

    up

--- platform.php
+++ platform.php
@@ -68,9 +68,10 @@ body, html {
 
 <section class="text-gray-700 body-font">
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Evidence based management of <br>HR+ Advance Breast Cancer</h1>
     <iframe src="https://video.ibm.com/embed/23951896" style="border: 0;" webkitallowfullscreen allowfullscreen frameborder="no" width="480" height="270"></iframe>
     <div class="text-center lg:w-2/3 w-full">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Evidence based management of HR+ Advance Breast Cancer</h1>
+      
       <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
